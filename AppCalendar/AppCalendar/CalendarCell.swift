81810cf4d8ba5fd1dab6992442ecddaa2b9e4b62
atran123123 <74937350+atran123123@users.noreply.github.com>
2023-05-25 11:05:16 -0400

    Add files via upload

---
+++ AppCalendar/AppCalendar/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  AppCalendar
+//
+//  Created by Alex Tran on 4/24/23.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
+
